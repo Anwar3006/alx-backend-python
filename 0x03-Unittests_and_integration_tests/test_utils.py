8e871ed32fcfca9ddbe4452ab1682669ab534bd1
Anwar3006 <anwarsadat.d2@gmail.com>
2023-05-20 22:05:14 +0000

    Unit testing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,9 +3,9 @@
 Unit Tests on Utils.py
 """
 import unittest
-from utils import access_nested_map
+import utils
 from parameterized import parameterized
-import sys
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -28,23 +28,43 @@ class TestAccessNestedMap(unittest.TestCase):
         self.nested_dict = input1
         self.path = input2
         self.assertEqual(
-            access_nested_map(
+            utils.access_nested_map(
                 self.nested_dict,
                 self.path),
             output)
-    
+
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, input1, input2):
         """
-        Checking the 
+        Checking the Error raised is as expected
         """
         self.nested_dict = input1
         self.path = input2
         with self.assertRaises(KeyError) as aR:
-            access_nested_map(input1, input2)
+            utils.access_nested_map(input1, input2)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Tests the `get_json` function.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        Test the get_json function's output
+        """
+        with patch('requests.get') as mock_req_get:
+            mock_data = Mock(return_value=test_payload)
+            mock_req_get.return_value.json = mock_data
+            self.assertEqual(utils.get_json(test_url), test_payload)
+            mock_req_get.assert_called_once_with(test_url)
+
 
 if __name__ == "__main__":
     unittest.main()
