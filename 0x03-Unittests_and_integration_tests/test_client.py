0fa528b497644c3cd291f9918f6de2f18b5220f2
Anwar3006 <anwarsadat.d2@gmail.com>
2023-05-22 13:23:05 +0000

    Unit testing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -48,5 +48,68 @@ class TestGithubOrgClient(unittest.TestCase):
                 "https://api.github.com/users/google/repos"
             )
 
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """
+        Testing GithubOrgClient.public_repos.
+        """
+        test_payload = {
+            'repos_url': "https://api.github.com/users/google/repos",
+            'repos': [
+                {
+                    "id": 460600860,
+                    "node_id": "R_kgDOG3Q2HA",
+                    "name": ".allstar",
+                    "full_name": "google/.allstar",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/.allstar",
+                    "created_at": "2022-02-17T20:40:32Z",
+                    "updated_at": "2023-04-03T17:58:33Z",
+                    "has_issues": True,
+                    "forks": 3,
+                    "default_branch": "main"
+                },
+                {
+                    "id": 170908616,
+                    "node_id": "MDEwOlJlcG9zaXRvcnkxNzA5MDg2MTY=",
+                    "name": ".github",
+                    "full_name": "google/.github",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/.github",
+                    "created_at": "2019-02-15T18:14:38Z",
+                    "updated_at": "2023-04-15T18:17:55Z",
+                    "has_issues": True,
+                    "forks": 194,
+                    "default_branch": "master"
+                }
+            ]
+        }
+        mock_get_json.return_value = test_payload['repos']
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_pub_repos:
+            mock_pub_repos.return_value = test_payload["repos_url"]
+            self.assertEqual(
+                GithubOrgClient("google").public_repos(),
+                [
+                    ".allstar",
+                    ".github",
+                ]
+            )
+            mock_pub_repos.assert_called_once()
+        mock_get_json.assert_called_once()
+
+
+
+            
 if __name__ == "__main__":
     unittest.main()
