c9387d51301e2b35c76fd2db609cc5bba1ec5336
Anwar3006 <anwarsadat.d2@gmail.com>
2023-05-22 11:58:04 +0000

    Unit testing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -32,6 +32,21 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org)
         )
+    
+    def test_public_repos_url(self):
+        """
+        Testing GithubOrgClient._public_repos_url
+        """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            print(GithubOrgClient("google")._public_repos_url)
+        self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos"
+            )
 
 if __name__ == "__main__":
     unittest.main()
